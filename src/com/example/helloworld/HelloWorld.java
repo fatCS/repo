5b8fe9b468d9910a7147de6da0e6a1975d279ae8
zeynep <zeynep2362>
2017-09-13 02:36:45 +0200

    first commit of HelloWorld.java

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,26 @@
+package com.example.helloworld;
+
+/**
+ * Created by zeynep on 13.05.2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+
+            System.out.println("Hello World!!! yead");
+
+          /* System.out.println("it's me, Wombat!");
+
+            System.out.println(divide(5,4));
+
+            Circle circle = new Circle(5);
+            Square square = new Square(4);
+
+            System.out.println("perimeter of the circle with radius " + circle.getRadius() + " is " + circle.getPerimeter());
+            System.out.println("perimeter of the square with edge " + square.getEdge() + " is " + square.getPerimeter());
+            System.out.println("area of the square with edge " + square.getEdge() + " is " + square.getArea());*/
+    }
+
+    public static int divide (int a, int b){
+        return (a/b);
+    }
+}
